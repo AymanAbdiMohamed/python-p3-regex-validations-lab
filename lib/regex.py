1fda687eff285156f7b8f1acd3e9ff93b3aacda8
Ayman Abdi Mohamed <9100aymanabdi@gmail.com>
2025-11-23 17:04:42 +0300

    completed lab

--- lib/regex.py
+++ lib/regex.py
@@ -1,13 +1,19 @@
 import re
 
-# NOTE: There are only a few tests included, so multiple solutions will work.
-# Feel free to encourage students to find oversights and add tests to this lab!
-
-name = r""
+# ------------------------------
+# Name regex: fully robust
+# ------------------------------
+name = r"^([A-Z][a-z]*(?:[-'][A-Z][a-z]*)?)( [A-Z][a-z]*(?:[-'][A-Z][a-z]*)?)*$"
 name_regex = re.compile(name)
 
-phone_number = r""
+# ------------------------------
+# Phone regex: robust
+# ------------------------------
+phone_number = r"^(\(\d{3}\)\s?|\d{3}[-. ]?)\d{3}[-. ]?\d{4}$"
 phone_regex = re.compile(phone_number)
 
-email_address = r""
+# ------------------------------
+# Email regex: must start with a letter
+# ------------------------------
+email_address = r"^[A-Za-z][\w\.-]*@[\w-]+(\.[\w-]+)+$"
 email_regex = re.compile(email_address)
